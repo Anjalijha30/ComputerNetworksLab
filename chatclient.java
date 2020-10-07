baa1e7fb571e7955199bcf8a383863956809e94f
Anjali Jha <ganjalijha2000@gmail.com>
2020-10-07 20:05:13 +0530

    Create chatclient.java

---
+++ chatclient.java
@@ -0,0 +1,36 @@
+package Project1;
+import java.io.*;
+import java.net.*;
+public class chatclient {
+
+	public static void main(String[] args) throws Exception{
+		Socket c=null;
+		DataInputStream usr_inp=null;
+		DataInputStream din=new DataInputStream(System.in);
+		DataOutputStream dout=null;
+		try{
+			c=new Socket("LocalHost",1234);
+			usr_inp =new DataInputStream(c.getInputStream());
+			dout=new DataOutputStream(c.getOutputStream());
+		}
+		catch(IOException e){}
+		if(c!=null || usr_inp!=null ||dout!=null){
+			String unip;
+			System.out.println("\nEnter the message for server:");
+			while((unip=din.readLine())!=null)
+			{
+				dout.writeBytes(""+unip);
+				dout.writeBytes("\n");
+				System.out.println("Message from Server:");
+				System.out.println(usr_inp.readLine());
+				System.out.println("\nEnter the message for server:");
+			}
+			System.exit(0);
+		}
+		din.close();
+		usr_inp.close();
+		c.close();
+
+	}
+
+}
